6d3bf7355bcf2f1b065250e0062d171f372aabe7
RashedKlo <RashedKlo@users.noreply.github.com>
2025-11-15 13:52:14 +0000

    deploy: f004e77daa72f4105a53c66618d014276b45cd55

---
+++ _next/static/chunks/app/blog/page-96a73393a8ec6b9a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404],{82241:function(e,n,s){Promise.resolve().then(s.t.bind(s,90413,23)),Promise.resolve().then(s.t.bind(s,68326,23))}},function(e){e.O(0,[326,413,971,472,744],function(){return e(e.s=82241)}),_N_E=e.O()}]);
